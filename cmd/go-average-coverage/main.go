7556f26f0cae5dd911a68a54079dac6f608f7b76
Johan Andersson <skagget77@gmail.com>
2020-06-23 09:25:33 +0200

    Transparently output all input.

--- cmd/go-average-coverage/main.go
+++ cmd/go-average-coverage/main.go
@@ -13,13 +13,13 @@ var covRex *regexp.Regexp = regexp.MustCompile(`coverage: (\d+\.?\d*)% of statem
 func main() {
 	coverages := make([]float64, 0, 16)
 	for in := bufio.NewScanner(os.Stdin); in.Scan(); {
+		fmt.Println(in.Text())
 		if matches := covRex.FindStringSubmatch(in.Text()); matches != nil {
 			coverage, err := strconv.ParseFloat(matches[1], 64)
 			if err != nil {
 				fmt.Printf("go-average-coverage: %v\n", err)
 				return
 			}
-
 			coverages = append(coverages, coverage)
 		}
 	}
